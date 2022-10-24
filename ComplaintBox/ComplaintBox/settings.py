6e37750d3152a8c965e0d8e9528e7e289ec3857d
eleensmathew <eleensmathew@gmail.com>
2022-10-24 19:24:48 +0530

    models complete

--- ComplaintBox/ComplaintBox/settings.py
+++ ComplaintBox/ComplaintBox/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user',
+    'django.contrib.postgres',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,3 +135,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#for images
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- ComplaintBox/ComplaintBox/urls.py
+++ ComplaintBox/ComplaintBox/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#image lines start
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    ...]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+#image lines end
 urlpatterns = [
     path('admin/', admin.site.urls),
     # to direct user to registration page & login page
-    path('user/', include("user.urls"))
+    #path('user/', include("user.urls"))
 ]
