df0e7fa1fd0ec829784088a56c536a324b5dd5ef
eleensmathew <eleensmathew@gmail.com>
2022-10-26 22:45:19 +0530

    resolved model bugs using from django.contrib.auth.models import User

--- ComplaintBox/user/models.py
+++ ComplaintBox/user/models.py
@@ -1,50 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MaxValueValidator
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
-from .manager import *
+#from .manager import *
 
 
 
+#after making changes to models we have to make migration
+#after this go to users admin.py and register models
+# class Profile(models.Model):
+#     user=models.OneToOneField(User,on_delete=models.CASCADE)
+#     image=models.ImageField(default='default.jpg',upload_to='profile_pics')  #images will get saved in directory called profile_pics
+#
+#     def __str__(self):
+#         return f'{self.user.username} Profile'  #will dispaly in a nice way otherwise will return object name
 
-class CustomAccountManager(BaseUserManager):
-    def create_user(self,Email,Name,Password,**other_fields):
-        other_fields.setdefault('is_active',True)
-        other_fields.setdefault('is_superuser',True)
-        
-        if other_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must be assigned to is_superuser=True.')
-        return self.create_user(Email,Name,Password,**other_fields)
 
-    def create_user(self,Email,Name,Password,**other_fields):
-        if not Email:
-            raise ValueError('You must provide an email address')
+
+# model containing user's data
+class UserProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     
-        email=self.normalize_email(email)
-        user=self.model(Email=Email,Name=Name,**other_fields)
-        user.set_password(Password)
-        user.save
-        return user
-
-#model containing user's data
-class UserProfile(AbstractBaseUser,PermissionsMixin):
-    #username :models.CharField(max_length=100)
-    Email : models.CharField(unique=True,max_length=99)
-    Name : models.CharField(max_length=100)
-    Address : models.TextField(default = None)
-    Phone_no : models.PositiveBigIntegerField(default = None)
-    Password = models.CharField(max_length=50, default=None)
-    Star : ArrayField(
-            models.DecimalField(blank=True,validators=[
-                                       MaxValueValidator(5)]),
-            size=2,
-        )
-    is_active:models.BooleanField(default=False)
-    Preference : models.JSONField(null=True, default=dict)
-    objects = CustomAccountManager()
-    USERNAME_FIELD = 'Email'
-    REQUIRED_FIELDS = ['name']
-    
+    image=models.ImageField(default='default.jpg',upload_to='profile_pics')  #images will get saved in directory called profile_pics
+    Star: ArrayField(
+        models.DecimalField(blank=True, validators=[
+            MaxValueValidator(5)]),
+        size=2,
+    )
+    Address: models.TextField(default=None)
+    Phone_no: models.PositiveBigIntegerField(default=None)
+    Preference: models.JSONField(null=True, default=dict)
     def __str__(self):
-         return self.Email
+        return f'{self.user.username} Profile'  #will dispaly in a nice way otherwise will return object name
+    
--- ComplaintBox/worker/models.py
+++ ComplaintBox/worker/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.models import AbstractUser
+
 from django.core.validators import MaxValueValidator
 from .manager import *
+from django.contrib.auth.models import User
 #model containing worker's data
-class WorkerProfile(AbstractUser):
-    username : models.CharField(max_length=100)
-    #Name : models.CharField(max_length=100)
+class WorkerProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    
     Bio : models.TextField(default = None)
     Phone_no : models.PositiveBigIntegerField(default = None)
-    # Email : models.EmailField(unique=True)
-    Password = models.CharField(max_length=50, default=None)
-    #to store present rating and number of users who have rated in a list.
-    #to access the values in the list use Star__0 and Star__1
     Star : ArrayField(
             models.DecimalField(blank=True,validators=[
                                        MaxValueValidator(5)]),
             size=2,
         )
     Profession : models.CharField(max_length=100)
-    USERNAME_FIELD = 'username'
+    
     
