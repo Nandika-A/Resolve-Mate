b5006af5c3dfd5c6a81b89ff1a2766df3142fb17
Nandika-A <nandikaagrawal610@gmail.com>
2022-10-27 16:34:15 +0530

    Made user and worker profile in one model

--- ComplaintBox/user/admin.py
+++ ComplaintBox/user/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Workermore, Usermore
 # Register your models here.
 admin.site.register(UserProfile)
--- ComplaintBox/user/models.py
+++ ComplaintBox/user/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+
 from django.contrib.auth.models import User
 
 
@@ -8,7 +8,6 @@ from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MaxValueValidator
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 #from .manager import *
-
 
 
 #after making changes to models we have to make migration
@@ -24,17 +23,72 @@ from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUse
 
 # model containing user's data
 class UserProfile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    #both user and worker data can be accessed using one table, just by defining roles
+    class Role(models.TextChoices):
+        USER = "USER", "User"
+        WORKER = "WORKER", "Worker"
     
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    base_role = Role.USER
+    role = models.CharField(max_length=50, choices = Role.choices)
     image=models.ImageField(default='default.jpg',upload_to='profile_pics')  #images will get saved in directory called profile_pics
     Star: ArrayField(
         models.DecimalField(blank=True, validators=[
             MaxValueValidator(5)]),
         size=2,
     )
-    Address: models.TextField(default=None)
+    #Address: models.TextField(default=None)
     Phone_no: models.PositiveBigIntegerField(default=None)
-    Preference: models.JSONField(null=True, default=dict)
+    #Preference: models.JSONField(null=True, default=dict)
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+        return super().save(*args, **kwargs)
     def __str__(self):
         return f'{self.user.username} Profile'  #will dispaly in a nice way otherwise will return object name
     
+class Usermanager(models.Manager): #to separate user and worker data.
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role = UserProfile.Role.USER) 
+    
+class UserData(UserProfile):
+    objects = Usermanager()
+    base_role = UserProfile.Role.USER
+    class Meta:
+        proxy = True #won't create a new table for user
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = UserProfile.Role.USER
+        return super().save(*args, **kwargs)
+    @property #to access additional fields in user model
+    def more(self):
+        return self.Usermore
+    
+class Usermore(models.Model):
+    user=models.OneToOneField(UserProfile,on_delete=models.CASCADE,null=True)
+    address : models.TextField(default = None)
+    preference: models.JSONField(null=True, default=dict)
+     
+class Workermanager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role = UserProfile.Role.WORKER) 
+    
+class WorkerData(UserProfile):
+    objects = Workermanager()
+    base_role = UserProfile.Role.WORKER
+    class Meta:
+        proxy = True
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = UserProfile.Role.USER
+        return super().save(*args, **kwargs)
+    @property
+    def more(self):
+        return self.Workermore
+    
+class Workermore(models.Model):
+    user=models.OneToOneField(UserProfile,on_delete=models.CASCADE,null=True)
+    biodata : models.TextField(default = None)
+    profession : models.CharField(max_length=100)
