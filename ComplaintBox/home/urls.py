c55638c69cc0cfcae938a437c36bc130048ae8a9
Nandika-A <nandikaagrawal610@gmail.com>
2022-10-29 11:01:46 +0530

    complaint form

--- ComplaintBox/home/urls.py
+++ ComplaintBox/home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homepage, name = 'homepage'),
     path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
         activate, name='activate'),
+    path('/form', views.complaintform, name = 'complaintform'),
 ]
