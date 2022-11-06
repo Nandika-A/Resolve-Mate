31dc16e4680bb74b222c14c9593ff1ffe0fbf2b4
eleensmathew <eleensmathew@gmail.com>
2022-11-07 02:22:21 +0530

    minor profile changes

--- ComplaintBox/ComplaintBox/settings.py
+++ ComplaintBox/ComplaintBox/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+AUTH_USER_MODEL = 'user.UserProfile'
 
 from pathlib import Path
 
