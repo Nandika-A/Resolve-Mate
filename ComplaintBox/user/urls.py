d13af9f82062ba6a5ced043171a85c13149dfea7
eleensmathew <eleensmathew@gmail.com>
2022-11-06 14:12:38 +0530

    merge

--- ComplaintBox/user/urls.py
+++ ComplaintBox/user/urls.py
@@ -6,21 +6,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     #To direct user to register page
-<<<<<<< HEAD
-    path('user_register/', user_views.register, name = 'register'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name = 'logout'),
-    path('profile/', user_views.profile, name= 'profile'),
-    path("password_reset", auth_views.password_reset_request, name="password_reset"),#**check if it is user or auth
-    path('edit_profile/', user_views.edit_profile, name= 'edit-profile'),
-=======
     path('/user_register/', user_views.register, name = 'register'),
     path('/login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'login'),
     path('/logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name = 'logout'),
     path('/profile/', user_views.profile, name= 'profile'),
     path('/password_reset', auth_views.password_reset_request, name="password_reset"),#**check if it is user or auth
     #path('edit_profile/', user_views.edit_profile, name= 'edit_profile'),
->>>>>>> 8feba73d58496b1b42cf29e6ebc60bf4d1d27a02
     path ('',include('home.urls')),
 ]
 #image
